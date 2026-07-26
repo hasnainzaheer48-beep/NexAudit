c5e54e24cb582d5af773c027246091c646c60bf8
Hasnain Zaheer <hasnainzaheer48@gmail.com>
2026-07-26 21:12:53 +0500

    feat: add users POST endpoint

--- backend/src/controllers/users.controller.js
+++ backend/src/controllers/users.controller.js
@@ -13,4 +13,24 @@ const getUsers = async (req, res) => {
     }
 }
 
-module.exports = { getUsers };
+const createUser = async (req, res) => {
+    try {
+        let { first_name,
+            last_name,
+            email,
+            password_hash,
+            role,
+            phone_number } = req.body;
+        let result = await pool.query('INSERT INTO users(first_name,last_name,email,password_hash,role,phone_number) values ($1,$2,$3,$4,$5,$6) RETURNING *',
+            [first_name, last_name, email, password_hash, role, phone_number]
+        );
+        res.json(result.rows[0]);
+    }
+
+    catch (error) {
+        console.error(error);
+        res.status(500).send("Could not Create User");
+    }
+}
+
+module.exports = { getUsers, createUser };
--- backend/src/routes/users.routes.js
+++ backend/src/routes/users.routes.js
@@ -1,7 +1,9 @@
-const { getUsers } = require('../controllers/users.controller');
+const { getUsers, createUser } = require('../controllers/users.controller');
 const express = require('express');
 const router = express.Router();
 
 router.get('/', getUsers);
+
+router.post('/', createUser);
 
 module.exports = router; 
