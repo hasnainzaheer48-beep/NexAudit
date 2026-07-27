df4c4ec3196896d5799fcf1672e060d3d9859623
Hasnain Zaheer <hasnainzaheer48@gmail.com>
2026-07-27 15:15:24 +0500

    feat: Built Template Task Router

---
+++ backend/src/routes/template.tasks.routes.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+const {
+    getTemplateTasks,
+    getTemplateTaskById,
+    createTemplateTask,
+    updateTemplateTask,
+    deleteTemplateTask } = require('../controllers/template.tasks.controller');
+
+
+router.get('/', getTemplateTasks);
+
+router.get('/:id', getTemplateTaskById);
+
+router.post('/', createTemplateTask);
+
+router.patch('/:id', updateTemplateTask);
+
+router.delete('/:id', deleteTemplateTask);
+
+module.exports = router;
+
