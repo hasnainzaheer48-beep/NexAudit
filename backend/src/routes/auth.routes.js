1933aaa62e1b347d94135b9519521bac5f7b696c
Hasnain Zaheer <hasnainzaheer48@gmail.com>
2026-07-29 00:32:56 +0500

    feat: Added JWT authentication and authorization

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,8 +1,10 @@
-const { login } = require('../controllers/auth.controller');
+const { login, getCurrentUser } = require('../controllers/auth.controller');
 const express = require('express');
 const router = express.Router();
+const { authenticate } = require('../middlewares/auth.middleware');
 
 
 router.post('/login', login);
+router.get('/me', authenticate, getCurrentUser);
 
 module.exports = router;
