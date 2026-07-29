7759fc50a706194d07fe16484499750ea97c6de3
Hasnain Zaheer <hasnainzaheer48@gmail.com>
2026-07-29 13:51:59 +0500

    feat: Built route for task assignment

--- backend/src/routes/tasks.routes.js
+++ backend/src/routes/tasks.routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { getTasks, getTaskById, getTasksByAudit, updateTask, deleteTask } = require('../controllers/tasks.controller');
+const { getTasks, getTaskById, getTasksByAudit, updateTask, deleteTask, assignAuditor } = require('../controllers/tasks.controller');
 const { authenticate } = require('../middlewares/auth.middleware');
 
 router.use(authenticate);
@@ -9,6 +9,7 @@ router.get('/', getTasks);
 router.get('/:id', getTaskById);
 router.get('/audit/:id/', getTasksByAudit);
 router.patch('/:id', updateTask);
+router.patch('/:id/assign', assignAuditor);
 router.delete('/:id', deleteTask);
 
 module.exports = router;
