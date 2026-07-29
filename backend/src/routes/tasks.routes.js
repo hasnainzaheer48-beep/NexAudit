8ca38759ec11318677a254b6b9c9f56900856d80
Hasnain Zaheer <hasnainzaheer48@gmail.com>
2026-07-29 13:55:22 +0500

    feat: added authorization for task assignment

--- backend/src/routes/tasks.routes.js
+++ backend/src/routes/tasks.routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { getTasks, getTaskById, getTasksByAudit, updateTask, deleteTask, assignAuditor } = require('../controllers/tasks.controller');
 const { authenticate } = require('../middlewares/auth.middleware');
+const { requireRole } = require('../middlewares/roles.middleware');
 
 router.use(authenticate);
 
@@ -9,7 +10,7 @@ router.get('/', getTasks);
 router.get('/:id', getTaskById);
 router.get('/audit/:id/', getTasksByAudit);
 router.patch('/:id', updateTask);
-router.patch('/:id/assign', assignAuditor);
+router.patch('/:id/assign', requireRole('MANAGER'), assignAuditor);
 router.delete('/:id', deleteTask);
 
 module.exports = router;
