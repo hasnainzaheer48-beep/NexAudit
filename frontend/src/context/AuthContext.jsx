e321e074373dae9c870a9711166437423cfa9c77
Hasnain Zaheer <hasnainzaheer48@gmail.com>
2026-08-03 01:41:02 +0500

    Implement authentication context and user session management

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -1,10 +1,24 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
+import api from '../api/axios';
 
 export const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
     let [token, setToken] = useState(localStorage.getItem("token"));
     let [user, setUser] = useState({});
+
+
+    useEffect(() => {
+        if (!token)
+            return
+        const userData = async () => {
+            const userData = await api.get('/api/auth/me');
+            setUser(userData.data);
+
+        }
+
+        userData();
+    }, [token]);
 
     return (
         <AuthContext.Provider value={{ token, setToken, user, setUser }}>
