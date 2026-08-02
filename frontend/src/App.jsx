d028de485be89587e169814725c040e570bb92cb
Hasnain Zaheer <hasnainzaheer48@gmail.com>
2026-08-02 17:54:52 +0500

    feat: added navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,29 +3,31 @@ import Login from './pages/loginPage'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Dashboard from './pages/dashboard'
 import ProtectedRoute from './components/ProtectedRoute'
+import Navbar from './components/layout/navbar'
 
 
 function App() {
 
 
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route
-          path="/login"
-          element={<Login />}
-        />
+    // <BrowserRouter>
+    //   <Routes>
+    //     <Route
+    //       path="/login"
+    //       element={<Login />}
+    //     />
 
-        <Route
-          path="/dashboard"
-          element={
-            <ProtectedRoute>
-              <Dashboard />
-            </ProtectedRoute>
-          } />
+    //     <Route
+    //       path="/dashboard"
+    //       element={
+    //         <ProtectedRoute>
+    //           <Dashboard />
+    //         </ProtectedRoute>
+    //       } />
 
-      </Routes>
-    </BrowserRouter>
+    //   </Routes>
+    // </BrowserRouter>
+    <Navbar />
   )
 }
 
---
+++ frontend/src/components/layout/navbar.jsx
@@ -0,0 +1,9 @@
+export default function Navbar() {
+    return (
+        <div>
+            <div><h1>NEXAUDIT</h1></div>
+            <div>USER</div>
+            <hr />
+        </div>
+    )
+}
