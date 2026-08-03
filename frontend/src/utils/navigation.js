a0411b551c85411df48511cd4484584131a97652
Hasnain Zaheer <hasnainzaheer48@gmail.com>
2026-08-03 16:18:36 +0500

    feat: made a navigation function

---
+++ frontend/src/utils/navigation.js
@@ -0,0 +1,97 @@
+const navigation =
+{
+    ADMIN: [
+        {
+            name: "Dashboard",
+            path: "/dashboard"
+        },
+        {
+            name: "Activity Log",
+            path: "/activity-log"
+        },
+        {
+            name: "Audits",
+            path: "/audits"
+        },
+        {
+            name: "Audit Templates",
+            path: "/audit-templates"
+        },
+        {
+            name: "Template Tasks",
+            path: "/template-tasks"
+        },
+        {
+            name: "Tasks",
+            path: "/tasks"
+        },
+        {
+            name: "Users",
+            path: "/users"
+        },
+        {
+            name: "Clients",
+            path: "/clients"
+        }
+
+
+
+    ],
+
+
+    MANAGER: [
+        {
+            name: "Dashboard",
+            path: "/dashboard"
+        },
+        {
+            name: "Activity Log",
+            path: "/activity-log"
+        },
+        {
+            name: "Audits",
+            path: "/audits"
+        },
+        {
+            name: "Audit Templates",
+            path: "/audit-templates"
+        },
+        {
+            name: "Template Tasks",
+            path: "/template-tasks"
+        },
+        {
+            name: "Tasks",
+            path: "/tasks"
+        },
+
+        {
+            name: "Clients",
+            path: "/clients"
+        }
+
+
+
+    ],
+
+    AUDITOR: [
+        {
+            name: "Dashboard",
+            path: "/dashboard"
+        },
+        {
+            name: "Audits",
+            path: "/audits/me"
+        },
+        {
+            name: "Tasks",
+            path: "/tasks"
+        },
+
+
+
+
+    ]
+}
+
+export default navigation;
