77f9970208731f93e0a068e96b3af6dbc5341d9e
Hasnain Zaheer <hasnainzaheer48@gmail.com>
2026-08-04 18:29:01 +0500

    feat: added custom hook

---
+++ frontend/src/hooks/useUsers.js
@@ -0,0 +1,35 @@
+import { useState, useEffect } from "react";
+import api from '../api/axios'
+
+export default function useUsers() {
+    const [users, setUsers] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null)
+
+    const getUsers = async () => {
+        try {
+            setLoading(true)
+            const result = await api.get('/api/users');
+            setUsers(result.data)
+            console.log(result.data);
+        }
+        catch (error) {
+            console.error(error);
+            setError(error.message);
+        }
+
+        finally {
+            setLoading(false);
+        }
+    }
+
+
+    useEffect(() => {
+        getUsers();
+
+    }, []);
+
+    return ({ users, getUsers, loading, error })
+
+
+}
--- frontend/src/pages/users.jsx
+++ frontend/src/pages/users.jsx
@@ -1,27 +1,14 @@
-import { useEffect, useState } from "react";
-import api from "../api/axios";
+import { useState } from "react";
 import CreateUserModal from "../components/users/createUserModal";
+import useUsers from "../hooks/useUsers";
 
 
 
 
 export default function Users() {
 
-    const [users, setUsers] = useState([]);
+    const { users, getUsers, loading, error } = useUsers();
     const [showModal, setShowModal] = useState(false);
-
-    const getUsers = async () => {
-        try {
-
-            const result = await api.get('/api/users');
-            setUsers(result.data)
-            console.log(result.data);
-        }
-        catch (error) {
-            console.error(error);
-        }
-    }
-
     const handleCreateUser = () => {
         setShowModal(true)
     }
@@ -31,14 +18,12 @@ export default function Users() {
 
 
 
-
-
-    useEffect(() => {
-        getUsers();
-
-    }, []);
-
-
+    if (loading) {
+        return <h1>Loading</h1>
+    }
+    if (error) {
+        return <h1>Error</h1>
+    }
 
 
     return (
@@ -56,22 +41,28 @@ export default function Users() {
                         <th className="border px-4 py-3">Email</th>
                         <th className="border px-4 py-3">Phone</th>
                         <th className="border px-4 py-3">Created At</th>
+                        <th className="border px-4 py-3">Action</th>
                     </tr>
                 </thead>
                 <tbody>
                     {
                         users?.map((user) => {
-                            return (<tr key={user.id}>
-                                <td className="border  px-4 py-3">{user.id}</td>
-                                <td className="border px-4 py-3">{user.first_name}</td>
-                                <td className="border px-4 py-3">{user.last_name}</td>
-                                <td className="border px-4 py-3">{user.role}</td>
-                                <td className="border px-4 py-3">{user.email}</td>
-                                <td className="border px-4 py-3">{user.phone_number}</td>
-                                <td className="border px-4 py-3">{new Date(user.created_at).toLocaleDateString()} </td>
-                            </tr>);
+                            return (<>
+                                <tr key={user.id}>
+                                    <td className="border  px-4 py-3">{user.id}</td>
+                                    <td className="border px-4 py-3">{user.first_name}</td>
+                                    <td className="border px-4 py-3">{user.last_name}</td>
+                                    <td className="border px-4 py-3">{user.role}</td>
+                                    <td className="border px-4 py-3">{user.email}</td>
+                                    <td className="border px-4 py-3">{user.phone_number}</td>
+                                    <td className="border px-4 py-3">{new Date(user.created_at).toLocaleDateString()} </td>
+                                    <td className="border px-4 py-3"><button>Edit</button></td>
+                                </tr>
+                            </>
+                            );
                         })
                     }
+
 
                 </tbody>
             </table>
