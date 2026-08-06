63eb7a1d2fea238028e59ff9ae58d18d7038e75f
Hasnain Zaheer <hasnainzaheer48@gmail.com>
2026-08-07 01:22:36 +0500

    fix: fixed activity log page

--- frontend/src/pages/activityLogs.jsx
+++ frontend/src/pages/activityLogs.jsx
@@ -1,7 +1,51 @@
-export default function ActivityLogs() {
-    return (
+import useActivityLogs from "../hooks/useActivityLogs";
 
-        <h1>ACTIVITY LOGS</h1>
+export default function ActivityLogs() {
+
+    const { activityLogs, getActivityLogs, error, loading } = useActivityLogs();
+    if (loading) return null;
+    if (error) return <div>{error}</div>
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th className="border px-4 py-3">Id</th>
+                    <th className="border px-4 py-3">Entity Id</th>
+                    <th className="border px-4 py-3">Entity Type</th>
+                    <th className="border px-4 py-3">Changed By</th>
+                    <th className="border px-4 py-3">Action</th>
+                    <th className="border px-4 py-3">Old Value</th>
+                    <th className="border px-4 py-3">New Value</th>
+                    <th className="border px-4 py-3">Created At</th>
+                </tr>
+            </thead>
+            <tbody>
+                {
+                    activityLogs.map((activityLog) => {
+                        return (
+                            <tr key={activityLog.id}>
+                                <td className="border px-4 py-3">{activityLog.id}</td>
+                                <td className="border px-4 py-3">{activityLog.entity_id}</td>
+                                <td className="border px-4 py-3">{activityLog.entity_type}</td>
+                                <td className="border px-4 py-3">{activityLog.changed_by}</td>
+                                <td className="border px-4 py-3">{activityLog.action}</td>
+                                <td className="border px-4 py-3">
+                                    <pre>
+                                        {activityLog.old_value ? JSON.stringify(activityLog.old_value, null, 2) : '-'}
+                                    </pre>
+                                </td>
+                                <td className="border px-4 py-3">
+                                    <pre>
+                                        {activityLog.new_value ? JSON.stringify(activityLog.new_value, null, 2) : '-'}
+                                    </pre>
+                                </td>
+                                <td className="border px-4 py-3">{new Date(activityLog.created_at).toLocaleDateString()}</td>
+                            </tr>
+                        );
+                    })
+                }
+            </tbody>
+        </table>
 
     );
 }
