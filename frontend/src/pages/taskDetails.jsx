1550d94f3fbbd5562bf0ed24594bd829c93a933f
Hasnain Zaheer <hasnainzaheer48@gmail.com>
2026-08-08 16:29:07 +0500

    feat: built hook for getting task by id

--- frontend/src/pages/taskDetails.jsx
+++ frontend/src/pages/taskDetails.jsx
@@ -1,8 +1,18 @@
 import { useParams } from "react-router-dom"
+import useTask from "../hooks/useTask";
 
 export default function TaskDetails() {
 
     const { taskId } = useParams();
+    const { loading, error, task, getTask } = useTask(taskId);
+
+    if (loading) {
+        return null;
+    }
+
+    if (error) {
+        return error;
+    }
 
     return (
         <div>Details of Task {taskId} </div>
