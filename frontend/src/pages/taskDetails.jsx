9d87a709ac5ef56d3af19abe791518c8d0fcd097
Hasnain Zaheer <hasnainzaheer48@gmail.com>
2026-08-08 16:55:27 +0500

    feat: added details in task deetail page

--- frontend/src/pages/taskDetails.jsx
+++ frontend/src/pages/taskDetails.jsx
@@ -15,6 +15,32 @@ export default function TaskDetails() {
     }
 
     return (
-        <div>Details of Task {taskId} </div>
+        <>
+            <div>Task Details</div><hr />
+            <div>
+                <div>Title: {task.title}</div><br />
+                <div>Company: {task.company}</div><br />
+                <div>Assigned Auditor: {task.assigned_auditor}</div><br />
+                <div>Description: {task.description}</div><br />
+                <div>Start Date: {task.start_date ? new Date(task.start_date).toLocaleDateString() : '-'}</div><br />
+                <div>Due Date: {task.due_date ? new Date(task.due_date).toLocaleDateString() : '-'}</div><br />
+                <div>Completed At: {task.completed ? new Date(task.complete_at).toLocaleDateString : "-"}</div><br />
+                <div>Created At: {new Date(task.created_at).toLocaleDateString()}</div><br />
+                <div>Updated At: {task.updated_at ? new Date(task.updated_at).toLocaleDateString() : '-'}</div><br />
+                <hr />
+                <div>Status: {task.status} </div><br />
+                <div>Priority: {task.priority}</div><br />
+                <hr />
+                <div>Docs</div><br />
+                <hr />
+                <div>Comments</div><br /><hr />
+                <div>More Details</div><br />
+                <div>Task Id: {task.id}</div><br />
+                <div>Audit Id:  {task.audit_id}</div><br />
+                <div>Template Task Id: {task.template_task_id} </div><br />
+                <div>Assigned Auditor Id:  {task.assigned_auditor_id}</div><br />
+
+            </div>
+        </>
     )
 }
