b998196862d0888d2ea4779e95b2b9b0475a22db
Hasnain Zaheer <hasnainzaheer48@gmail.com>
2026-08-09 01:22:04 +0500

    feat: built controller for opening document

--- backend/src/controllers/document.controller.js
+++ backend/src/controllers/document.controller.js
@@ -73,4 +73,22 @@ const getDocumentsByTask = async (req, res) => {
     }
 }
 
-module.exports = { uploadDocument, getDocumentsByTask }
+const getDocument = async (req, res) => {
+
+    try {
+        const { id } = req.params;
+        const result = await pool.query(`SELECT * FROM documents WHERE id = $1`, [id]);
+        if (result.rows.length === 0) {
+            return res.send("This document does not exist");
+        }
+        const document = result.rows[0];
+        return res.sendFile(document.file_path)
+
+    }
+    catch (error) {
+        console.error(error)
+        return res.status(500).send('Could Not Open Document');
+    }
+}
+
+module.exports = { uploadDocument, getDocumentsByTask, getDocument }
