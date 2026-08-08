a8437aa348681da9d53bb408289f14f23a75202b
Hasnain Zaheer <hasnainzaheer48@gmail.com>
2026-08-08 18:00:03 +0500

    feat: created middleware for multer

---
+++ backend/src/middlewares/upload.middleware.js
@@ -0,0 +1,19 @@
+const multer = require('multer');
+const path = require('path');
+
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, path.join(__dirname, '../../public/docs'))
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + '-' + file.originalname)
+    }
+})
+
+const upload = multer({
+    storage
+})
+
+
+module.exports = upload;
