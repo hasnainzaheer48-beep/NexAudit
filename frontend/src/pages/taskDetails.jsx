7867f5270379a87f9a811b0ec5fecc63df235585
Hasnain Zaheer <hasnainzaheer48@gmail.com>
2026-08-09 17:48:19 +0500

    Feat: only Auditor can upload Docs for task

--- frontend/src/pages/taskDetails.jsx
+++ frontend/src/pages/taskDetails.jsx
@@ -3,13 +3,15 @@ import useTask from "../hooks/useTask";
 import UploadDocumentFormModal from "../components/documents/uploadDocumentFormModal";
 import { useState } from "react";
 import ShowTaskDocuments from "../components/documents/showTaskDocuments";
-
+import { useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
 
 export default function TaskDetails() {
 
     const { taskId } = useParams();
     const { loading, error, task, getTask } = useTask(taskId);
     const [showModal, setShowModal] = useState(false);
+    const { user } = useContext(AuthContext);
 
 
     const handleUpload = () => {
@@ -56,7 +58,7 @@ export default function TaskDetails() {
                 {/* Documents Upload and View */}
                 <div>Documents</div>
                 <ShowTaskDocuments taskId={taskId} />
-                <button onClick={handleUpload} className="border p-1 mb-2">Add Docs</button><br />
+                {(user.role === "AUDITOR") && <button onClick={handleUpload} className="border p-1 mb-2">Add Docs</button>}<br />
                 <UploadDocumentFormModal isOpen={showModal} onClose={handleClose} taskId={taskId} onUploaded={getTask} />
                 <hr />
                 {/* ------------------------------------------------- */}
