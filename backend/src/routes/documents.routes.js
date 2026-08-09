efb4d0ff5887fa74c3db065bf4c1a03be9f69cb6
Hasnain Zaheer <hasnainzaheer48@gmail.com>
2026-08-09 15:13:31 +0500

    Add document management and access control

--- backend/src/routes/documents.routes.js
+++ backend/src/routes/documents.routes.js
@@ -3,11 +3,12 @@ const { getDocument, downloadDocument } = require('../controllers/document.contr
 const express = require('express');
 const { authenticate } = require('../middlewares/auth.middleware');
 const { requireRole } = require('../middlewares/roles.middleware');
+const { validateDocumentAccess } = require('../middlewares/validateDocumentAccess.middleware');
 const router = express.Router();
 
 router.use(authenticate);
 
-router.get('/:id/open', getDocument);
-router.get('/:id/download', downloadDocument);
+router.get('/:id/open', validateDocumentAccess, getDocument);
+router.get('/:id/download', validateDocumentAccess, downloadDocument);
 
 module.exports = router;
