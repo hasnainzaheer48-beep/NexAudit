68c95c7766df9d63ad58e9ab72dcab3399cf710c
Hasnain Zaheer <hasnainzaheer48@gmail.com>
2026-08-11 15:23:27 +0500

    feat: built hook for getting recent activity

---
+++ frontend/src/hooks/useRecentActivity.js
@@ -0,0 +1,39 @@
+import { useState, useEffect } from "react";
+import api from "../api/axios";
+
+
+
+export default function useRecentActivity() {
+    const [recentAcitivty, setRecentActivity] = useState({});
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    const getRecentActivity = async () => {
+        try {
+            setLoading(true);
+            const result = await api.get(`/api/activity-logs/user`);
+            setRecentActivity(result.data);
+
+        }
+        catch (error) {
+            console.error(error);
+            setError(error.message);
+        }
+        finally {
+            setLoading(false);
+        }
+    }
+
+
+    useEffect(() => {
+        getRecentActivity();
+    }, [])
+
+    return (
+        { loading, error, recentAcitivty, getRecentActivity }
+    );
+
+
+
+
+}
