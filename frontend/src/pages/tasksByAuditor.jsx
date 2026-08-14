fa438ecff28c07c4ef8ae26aa14b387217ed9882
Hasnain Zaheer <hasnainzaheer48@gmail.com>
2026-08-14 15:30:11 +0500

    feat: updated UI of Auditor Tasks page

--- frontend/src/pages/tasksByAuditor.jsx
+++ frontend/src/pages/tasksByAuditor.jsx
@@ -2,6 +2,16 @@ import { useState } from "react";
 import useTasksByAuditor from "../hooks/useTasksByAuditor"
 import TasksByAuditorFormModal from "../components/tasksByAuditor/tasksByAuditorFormModal";
 import { useNavigate } from "react-router-dom";
+import LoadingComponent from "../components/ui/loadingComponent";
+import PageTitle from "../components/ui/pageTitle";
+import Table from "../components/ui/table/table";
+import TableHeader from "../components/ui/table/tableHeader";
+import TableRow from "../components/ui/table/tableRow";
+import TableHead from "../components/ui/table/tableHead";
+import TableCell from "../components/ui/table/tableCell";
+import PriorityBadge from "../components/ui/table/priorityBadge";
+import StatusBadge from "../components/ui/table/statusBadge";
+import EmptyTable from "../components/ui/table/emptyTable";
 
 
 export default function TasksByAuditor() {
@@ -17,7 +27,7 @@ export default function TasksByAuditor() {
     }
 
     if (loading) {
-        return <h1>Loading</h1>
+        return <LoadingComponent />
     }
 
     if (error) {
@@ -28,59 +38,61 @@ export default function TasksByAuditor() {
 
 
     return (
-        <div>
+        <div className="flex flex-col h-full">
+            <PageTitle title={"My Tasks"} />
+            <div className="flex-1 min-h-0">
+
+                <Table className="mt-4">
+                    <TableHeader>
+                        <TableRow>
+                            <TableHead>Id</TableHead>
+                            <TableHead>Title</TableHead>
+                            <TableHead>Audit Id</TableHead>
+                            <TableHead>Company</TableHead>
+                            <TableHead>Priority</TableHead>
+                            <TableHead>Status</TableHead>
+                            <TableHead>Start Date</TableHead>
+                            <TableHead>Due Date</TableHead>
+                            <TableHead>Completed At</TableHead>
+                            <TableHead>Action</TableHead>
+                        </TableRow>
+                    </TableHeader>
+                    <tbody>
+                        {tasks.length === 0 ? <EmptyTable /> :
+                            tasks.map((task) => {
+                                return (
+                                    <TableRow key={task.id}>
+                                        <TableCell>{task.id}</TableCell>
+                                        <TableCell>{task.title}</TableCell>
+                                        <TableCell>{task.audit_id}</TableCell>
+
+                                        <TableCell>{task.company}</TableCell>
 
 
-            <table className="mt-4">
-                <thead>
-                    <tr>
-                        <th className="border px-4 py-3">Id</th>
-                        <th className="border px-4 py-3">Title</th>
-                        <th className="border px-4 py-3">Audit Id</th>
-                        <th className="border px-4 py-3">Company</th>
-                        <th className="border px-4 py-3">Priority</th>
-                        <th className="border px-4 py-3">Status</th>
-                        <th className="border px-4 py-3">Start Date</th>
-                        <th className="border px-4 py-3">Due Date</th>
-                        <th className="border px-4 py-3">Completed At</th>
-                        <th className="border px-4 py-3">Action</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                        tasks.map((task) => {
-                            return (
-                                <tr key={task.id}>
-                                    <td className="border px-4 py-3">{task.id}</td>
-                                    <td className="border px-4 py-3">{task.title}</td>
-                                    <td className="border px-4 py-3">{task.audit_id}</td>
+                                        <TableCell><PriorityBadge priority={task.priority} /></TableCell>
+                                        <TableCell>< StatusBadge status={task.status} /></TableCell>
+                                        <TableCell>{task.start_date ? new Date(task.start_date).toLocaleDateString() : 'Null'}</TableCell>
+                                        <TableCell>{task.due_date ? new Date(task.due_date).toLocaleDateString() : 'Null'}</TableCell>
+                                        <TableCell>{task.completed_at ? new Date(task.completed_at).toLocaleDateString() : 'Null'}</TableCell>
 
-                                    <td className="border px-4 py-3">{task.company}</td>
-
-
-                                    <td className="border px-4 py-3">{task.priority}</td>
-                                    <td className="border px-4 py-3">{task.status}</td>
-                                    <td className="border px-4 py-3">{task.start_date ? new Date(task.start_date).toLocaleDateString() : 'Null'}</td>
-                                    <td className="border px-4 py-3">{task.due_date ? new Date(task.due_date).toLocaleDateString() : 'Null'}</td>
-                                    <td className="border px-4 py-3">{task.completed_at ? new Date(task.completed_at).toLocaleDateString() : 'Null'}</td>
-
-                                    <td className="border px-4 py-3"><button className="border px-2 mr-1" onClick={() => {
-                                        setSelectedTask(task);
-                                        setShowModal(true);
-                                    }}>Edit Status</button>
-                                        <button className="border px-2 mr-1" onClick={() => navigate(`/tasks/task-details/${task.id}`)}>View</button></td>
-                                </tr>
-                            );
-                        })
-                    }
-                </tbody>
-            </table>
-            <TasksByAuditorFormModal
-                isOpen={showModal}
-                selectedTask={selectedTask}
-                onClose={handleClose}
-                onTaskUpdated={getTasksByAuditor}
-            />
+                                        <TableCell><button className="border px-2 mr-1" onClick={() => {
+                                            setSelectedTask(task);
+                                            setShowModal(true);
+                                        }}>Edit Status</button>
+                                            <button className="border px-2 mr-1" onClick={() => navigate(`/tasks/task-details/${task.id}`)}>View</button></TableCell>
+                                    </TableRow>
+                                );
+                            })
+                        }
+                    </tbody>
+                </Table>
+                <TasksByAuditorFormModal
+                    isOpen={showModal}
+                    selectedTask={selectedTask}
+                    onClose={handleClose}
+                    onTaskUpdated={getTasksByAuditor}
+                />
+            </div>
         </div>
 
 
