2d5ff77577e683c43f687f34885843c3e6ee3e06
Hasnain Zaheer <hasnainzaheer48@gmail.com>
2026-08-16 01:57:05 +0500

    feat: added button to go back to tasks

--- frontend/src/pages/taskDetails.jsx
+++ frontend/src/pages/taskDetails.jsx
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom"
+import { useNavigate, useParams } from "react-router-dom"
 import UploadDocumentFormModal from "../components/documents/uploadDocumentFormModal";
 import { useState } from "react";
 import ShowTaskDocuments from "../components/documents/showTaskDocuments";
@@ -15,6 +15,7 @@ export default function TaskDetails() {
     const { taskId } = useParams();
     const { loading, error, task, getTask } = useTaskById(taskId);
     const { user } = useContext(AuthContext);
+    const navigate = useNavigate();
 
 
     if (loading) {
@@ -27,6 +28,9 @@ export default function TaskDetails() {
 
     return (
         <>
+            <div>
+                <button onClick={() => navigate(-1)} className="font-bold text-xs hover:cursor-pointer hover:underline text-[#174d38] tracking-wide ">Go Back</button>
+            </div>
             <div>Task Details</div><hr />
             <div>
 
