d9d6db63132b1e562a42d7edfb381132d5bfc613
Hasnain Zaheer <hasnainzaheer48@gmail.com>
2026-08-17 15:18:32 +0500

    feat: Updated UI of Documents Form

--- frontend/src/components/documents/uploadDocumentFormModal.jsx
+++ frontend/src/components/documents/uploadDocumentFormModal.jsx
@@ -1,6 +1,10 @@
 import { useEffect, useState } from 'react';
 import Modal from '../ui/Modal'
 import api from '../../api/axios';
+import FormInput from '../ui/form/formInput';
+import FormActions from '../ui/form/formActions';
+import FormField from '../ui/form/formField';
+
 
 
 export default function UploadDocumentFormModal({ isOpen, onClose, taskId, onUploaded }) {
@@ -53,14 +57,14 @@ export default function UploadDocumentFormModal({ isOpen, onClose, taskId, onUpl
         <Modal title={"Upload Document"}
             subtitle={"Attach a document to this task"}
             onClose={onClose} size="xl">
-            <form onSubmit={handleSubmit}>
-                <label>
-                    <input className='border' type="file" name='file' onChange={handleFile} required />
-                </label>
-                <label>
-                    Description <input type="text" name="description" value={description} onChange={handleDescription} />
-                </label>
-                <button>Upload</button>
+            <form onSubmit={handleSubmit} className="space-y-4 py-3">
+                <FormField label={"Upload Document"}>
+                    <FormInput className='border' type="file" name='file' onChange={handleFile} required />
+                </FormField>
+                <FormField label="Description">
+                    <FormInput type="text" name="description" value={description} onChange={handleDescription} />
+                </FormField>
+                <FormActions submitText='Upload Document' onClose={onClose} />
             </form>
 
 
