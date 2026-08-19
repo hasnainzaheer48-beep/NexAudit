14d34f6cf3bdbb58e833e43ec1fd6ab2c47b7cd7
Hasnain Zaheer <hasnainzaheer48@gmail.com>
2026-08-19 12:45:25 +0500

    feat: added edit button in Task Details

--- frontend/src/pages/taskDetails.jsx
+++ frontend/src/pages/taskDetails.jsx
@@ -1,17 +1,15 @@
 import { useNavigate, useParams } from "react-router-dom"
-
-
-
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { AuthContext } from "../context/AuthContext";
-
 import Documents from "../components/documents/documents";
-
 import Comments from "../components/comments/comments";
 import useTaskById from "../hooks/useTaskById";
 import TaskTitle from "../components/taskDetails/taskTitle";
 import TaskInfoCard from "../components/taskDetails/taskInfoCard";
 import ExtraDetails from "../components/taskDetails/extraDetails";
+import Button from "../components/ui/button";
+import TasksFormModal from "../components/tasks/tasksFormModal";
+
 
 export default function TaskDetails() {
 
@@ -19,6 +17,12 @@ export default function TaskDetails() {
     const { loading, error, task, getTask } = useTaskById(taskId);
     const { user } = useContext(AuthContext);
     const navigate = useNavigate();
+    const [showModal, setShowModal] = useState(false);
+
+    const handleClose = () => {
+        setShowModal(false);
+
+    }
 
 
     if (loading) {
@@ -31,8 +35,9 @@ export default function TaskDetails() {
 
     return (
         <div className="flex flex-col gap-5 pb-6  min-h-full">
-            <div>
+            <div className="flex justify-between items-center">
                 <button onClick={() => navigate(-1)} className="font-bold text-xs hover:cursor-pointer hover:underline text-[#174d38] tracking-wide ">Go Back</button>
+                {user.role === "MANAGER" && <Button variant="Edit" size="Normal" icon="Edit" iconSize="Small" onClick={() => { setShowModal(true) }} >Edit</Button>}
             </div>
             <TaskTitle title={task.title} status={task.status} />
             <div>
@@ -50,7 +55,12 @@ export default function TaskDetails() {
             <div>
                 <ExtraDetails task={task} />
             </div>
-
+            <TasksFormModal
+                isOpen={showModal}
+                onClose={handleClose}
+                selectedTask={task}
+                onTaskCreated={getTask}
+            />
 
         </div>
     )
