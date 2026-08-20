246bae96d71760cffa47b5153f14bfa46eb42a91
Hasnain Zaheer <hasnainzaheer48@gmail.com>
2026-08-20 14:48:41 +0500

    feat: added pagination in Clients

--- frontend/src/hooks/useClients.js
+++ frontend/src/hooks/useClients.js
@@ -1,18 +1,24 @@
 import api from '../api/axios'
 import { useState, useEffect } from 'react';
 
-export default function useClients() {
+export default function useClients(page = 1, limit = 10) {
 
     const [clients, setClients] = useState([]);
+    const [pagination, setPagination] = useState(null)
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null)
 
     const getClients = async () => {
         try {
             setLoading(true);
-            const result = await api.get('/api/clients');
-            setClients(result.data);
-
+            const result = await api.get('/api/clients', {
+                params: {
+                    page,
+                    limit
+                }
+            });
+            setClients(result.data.data);
+            setPagination(result.data.pagination)
         }
         catch (error) {
             console.error(error);
@@ -29,7 +35,7 @@ export default function useClients() {
     }, [])
 
     return (
-        { loading, error, clients, getClients }
+        { loading, pagination, error, clients, getClients }
     );
 
 }
